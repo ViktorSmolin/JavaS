b6b30865066f9f3b6c6741ccc4eee848b63acb7c
Viktor Smolin <54165337+ViktorSmolin@users.noreply.github.com>
2022-07-02 17:10:29 +0700

    Update YandexMap.js

--- YandexMap.js
+++ YandexMap.js
@@ -1,11 +1,57 @@
+const arr = [1, 5, 8, 6, 2];
 
-let fruit = prompt("Какой фрукт купить?", "");
-
-let bag = {
-  [fruit]: 5, 
-  [fruit]: 4, 
-  [fruit]: 3, 
+const d = (arr) => {
+  return arr.map((item) => item * 2);
 };
 
-alert( bag.q ); 
+console.log(d(arr));
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+
+function init() {
+  // Создание карты.
+  // https://tech.yandex.ru/maps/doc/jsapi/2.1/dg/concepts/map-docpage/
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчнию: «широта, долгота».
+    center: [55.76, 37.64],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 12,
+    // Элементы управления
+    // https://tech.yandex.ru/maps/doc/jsapi/2.1/dg/concepts/controls/standard-docpage/
+    controls: [
+      "zoomControl", // Ползунок масштаба
+      "rulerControl", // Линейка
+      "routeButtonControl", // Панель маршрутизации
+      "trafficControl", // Пробки
+      "typeSelector", // Переключатель слоев карты
+      "fullscreenControl", // Полноэкранный режим
+
+      // Поисковая строка
+      new ymaps.control.SearchControl({
+        options: {
+          // вид - поисковая строка
+          size: "large",
+          // Включим возможность искать не только топонимы, но и организации.
+          provider: "yandex#search",
+        },
+      }),
+    ],
+  });
+
+  // Добавление метки
+  // https://tech.yandex.ru/maps/doc/jsapi/2.1/ref/reference/Placemark-docpage/
+  var myPlacemark = new ymaps.Placemark([55.76, 37.64], {
+    // Хинт показывается при наведении мышкой на иконку метки.
+    hintContent: "Содержимое всплывающей подсказки",
+    // Балун откроется при клике по метке.
+    balloonContent: "Содержимое балуна",
+  });
+
+  // После того как метка была создана, добавляем её на карту.
+  myMap.geoObjects.add(myPlacemark);
+}
 
